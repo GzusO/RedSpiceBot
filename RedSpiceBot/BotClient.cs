693cf8487912565946268e5c74ca2f8aeec47985
gagnonblake <gagnon.blake@gmail.com>
2020-10-17 01:54:16 -0500

    Red spice storage and shinier chat commands

--- RedSpiceBot/BotClient.cs
+++ RedSpiceBot/BotClient.cs
@@ -10,6 +10,8 @@ using TwitchLib.Communication.Clients;
 using TwitchLib.Communication.Models;
 using TwitchLib.PubSub;
 using TwitchLib.PubSub.Events;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace RedSpiceBot
 {
@@ -29,12 +31,20 @@ namespace RedSpiceBot
     {
         TwitchClient botClient;
         TwitchPubSub botPubSub;
-        ConfigLoader.Info configInfo;
+        ConfigInfo configInfo;
+
+        #region Chat Strings
+        private const string SpiceBotReply = "Buy !RedSpice with channel points! " +
+                        "Check your red spice stores with !MySpice.";
+        private const string RedSpiceReply = "Earn Red Spice today! " +
+                        "Trade it for rare artifacts sometime in the future!";
+        private const string MySpiceReply = "{0}, you have {1} Red Spice.";
+        #endregion
 
         public Bot()
         {
             // Get connection info from config.json and initialize channels
-            configInfo = ConfigLoader.LoadInfo();
+            configInfo = LoadInfo();
             ConnectionCredentials credentials = new ConnectionCredentials(configInfo.identity.username, configInfo.identity.password);
             var clientOptions = new ClientOptions
             {
@@ -96,10 +106,30 @@ namespace RedSpiceBot
         {
             switch (e.Command.CommandText.ToLower())
             {
-                case "redspice":
-                    Console.WriteLine("!Redspice command received.");
-                    botClient.SendMessage(e.Command.ChatMessage.Channel, "Earn Redspice today!");
+                case "spicebot":
+                    Console.WriteLine("!SpiceBot command received.");
+                    botClient.SendMessage(e.Command.ChatMessage.Channel, SpiceBotReply);
                     break;
+
+                case "redspice":
+                    Console.WriteLine("!RedSpice command received.");
+                    botClient.SendMessage(e.Command.ChatMessage.Channel, RedSpiceReply);
+                    break;
+
+                case "myspice":
+                    Console.WriteLine("!MySpice command received.");
+                    Dictionary<string, UserStorage> storage = LoadStorage();
+                    if (storage.TryGetValue(e.Command.ChatMessage.Username, out UserStorage userStorage))
+                    {   
+                        botClient.SendMessage(e.Command.ChatMessage.Channel, string.Format(MySpiceReply, e.Command.ChatMessage.DisplayName, userStorage.spice));
+                    }
+                    else
+                    {
+                        // If a user doesn't have an entry in the storage database then they have never acquired spice, so they have 0
+                        botClient.SendMessage(e.Command.ChatMessage.Channel, string.Format(MySpiceReply, e.Command.ChatMessage.DisplayName, 0));
+                    }
+                    break;
+
                 default:
                     Console.WriteLine($"Received unknown command: {e.Command.CommandText}, from {e.Command.ChatMessage.Username}.");
                     break;
@@ -151,40 +181,114 @@ namespace RedSpiceBot
 
         private void OnRewardRedeemed(object sender, OnRewardRedeemedArgs e)
         {
-            Console.WriteLine("Points redeemed");
-            Console.WriteLine(e.DisplayName);
+            // Check if the reward is a buy spice reward in the format ... (xN) where N is amount of spice bought
+            if (e.Status == "ACTION_TAKEN")
+            {
+                Regex rxCheck = new Regex(@"\d+",
+                    RegexOptions.Compiled | RegexOptions.IgnoreCase);
+                MatchCollection matches = rxCheck.Matches(e.RewardTitle);
+                foreach (Match match in matches)
+                {
+                    int amount = Int32.Parse(match.Value);
+                    UpdateSpiceStorage(e.Login, amount);
+                }
+            }
+
         }
         #endregion
-    }
 
-    /*
-    * Static class that exists solely to load username and authentication token from the config JSON file
-    */
-    class ConfigLoader
-    {
-        static public Info LoadInfo()
+        #region Helpers
+        private ConfigInfo LoadInfo()
         {
             using (StreamReader r = new StreamReader("../../Config/config.json"))
             {
                 string config = r.ReadToEnd();
-                return JsonConvert.DeserializeObject<Info>(config);
+                r.Close();
+                return JsonConvert.DeserializeObject<ConfigInfo>(config);
             }
         }
 
-        public class Info
+        private Dictionary<string, UserStorage> LoadStorage()
         {
-            public Identity identity;
-            public string[] channels;
-            public string accessToken;
-            public string clientID;
-            public string refreshToken;
+            using (StreamReader r = new StreamReader("../../SpiceStorage/storage.json"))
+            {
+                string storageString = r.ReadToEnd();
+                r.Close();
+                return JsonConvert.DeserializeObject<Dictionary<string, UserStorage>>(storageString);
+            }
         }
 
-        public class Identity
+        /*
+         * Updates a users spice amount, returning whether or not the change is legal
+         * Only updates the spice count on a legal request
+         * Probably has race conditions and other weird bugs, but those are future me's problems
+         */
+        private bool UpdateSpiceStorage(string login, int spiceChange)
         {
-            public string username;
-            public string password;
+            bool isLegal = false;
+            Dictionary<string, UserStorage> storage = LoadStorage();
+
+            // If the storage doesn't exist at all yet, initialize it
+            if (storage == null)
+            {
+                storage = new Dictionary<string, UserStorage>();
+            }
+
+            // Update the user's spice count
+            UserStorage curStorage;
+            if (storage.TryGetValue(login, out curStorage))
+            {
+                // The user already has a storage
+                if ((curStorage.spice + spiceChange) >= 0)
+                {
+                    storage[login].spice = curStorage.spice + spiceChange;
+                    isLegal = true;
+                }
+            }
+            else
+            {
+                // The user does not have a storage set up
+                if (spiceChange > 0)
+                {
+                    UserStorage newStorage = new UserStorage();
+                    newStorage.spice = spiceChange;
+                    storage.Add(login, newStorage);
+                    isLegal = true;
+                }
+            }
+
+            // Save the changes back to the spice storage
+            // Probably dangerous to do this cause race conditions, should look into a way to prevent that
+            File.WriteAllText(@"../../SpiceStorage/storage.json", JsonConvert.SerializeObject(storage));
+
+            return isLegal;
         }
+        #endregion
     }
-    
+
+    #region Config Classes
+    public class ConfigInfo
+    {
+        public ConfigIdentity identity;
+        public string[] channels;
+        public string accessToken;
+        public string clientID;
+        public string refreshToken;
+    }
+
+    public class ConfigIdentity
+    {
+        public string username;
+        public string password;
+    }
+    #endregion
+
+    #region Storage Classes
+    // Storage obect exists as a login-keyed dictionary of UserStorage objects
+    public class UserStorage
+    {
+        public int spice;
+        public string[] artifacts;
+    }
+    #endregion
 }
